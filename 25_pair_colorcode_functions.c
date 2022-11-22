ebe9dd1b5a8d49c7785ca5bd12fe1592f65fccad
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-11-22 21:47:06 +0530

    Create 25_pair_colorcode_functions.c

---
+++ 25_pair_colorcode_functions.c
@@ -0,0 +1,51 @@
+#include "25_pair_colorcode_functions.h"
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
+
+void printtable() {
+    printf("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n");
+    printf("No \t \t Major color \t \t Minor color\n ");
+    printf("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n");
+    for (int pairNumber = 0;pairNumber < 25; pairNumber++) {
+       printf("%d \t", pairNumber+1);
+       if (pairNumber % 5 == 0)       
+           printf("\t %s ", MajorColorNames[pairNumber / 5]);
+        else 
+            printf("\t");
+     
+       printf("\t \t \t %s \t", MinorColorNames[pairNumber % 5]);
+       printf("\n");
+    }
+     printf("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n");
+}
