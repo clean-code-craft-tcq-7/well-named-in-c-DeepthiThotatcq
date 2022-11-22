c31b1d062632c1b2029389098c81afa003304886
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-11-22 10:40:16 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "colorcode_configuration.h"
+#include "main.h"
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
  sprintf(buffer, "%s %s",
  MajorColorNames[colorPair->majorColor],
