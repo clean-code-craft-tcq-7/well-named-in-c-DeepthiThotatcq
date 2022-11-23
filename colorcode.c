957abbfaf9338f7cc962164078a1ab421c17861f
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-11-23 12:42:14 +0530

    Update colorcode.c

--- colorcode.c
+++ colorcode.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
-#include "decom.h"
 #include "telecom colorcode.h"
+#include "decom.h"
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
