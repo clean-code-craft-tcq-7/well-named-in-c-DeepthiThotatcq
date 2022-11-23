1c4a0c5925af9095b1b569903c24770ce2a5e648
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-11-23 11:24:46 +0530

    Update colorcode.c

--- colorcode.c
+++ colorcode.c
@@ -34,7 +34,7 @@ int main() {
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    printf("         telecomunication color code table         ");
+    printf("          Telecomunication color code table         \n");
     printcolorcode();
     return 0;
 }
