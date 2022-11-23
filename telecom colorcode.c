8ff12eae2671f667c25f5b5a3a0e4e4cebbc9fed
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-11-23 09:48:35 +0530

    Update telecom colorcode.c

--- telecom colorcode.c
+++ telecom colorcode.c
@@ -33,10 +33,9 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
 
 void printtable() {
     printf("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n");
-    printf("No \t \t Major color \t \t Minor color\n ");
+    printf("Major color \t \t Minor color\n ");
     printf("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n");
     for (int pairNumber = 0;pairNumber < 25; pairNumber++) {
-       printf("%d", pairNumber+1);
        if (pairNumber % 5 == 0)       
            printf("%s ", MajorColorNames[pairNumber / 5]);
         else 
